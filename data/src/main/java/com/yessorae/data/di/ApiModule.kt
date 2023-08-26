ca3dd6b095849cf7859d98cf086b3a2fd1d84443
노소래 <yessorae@gmail.com>
2023-08-26 17:22:11 +0900

    fix: InstallIn 누락 추가

--- data/src/main/java/com/yessorae/data/di/ApiModule.kt
+++ data/src/main/java/com/yessorae/data/di/ApiModule.kt
@@ -3,9 +3,12 @@ package com.yessorae.data.di
 import com.yessorae.data.api.TxtToImgApi
 import dagger.Module
 import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 
 @Module
+@InstallIn(SingletonComponent::class)
 object ApiModule {
     @Provides
     fun provideTxtToImgApi(retrofit: Retrofit): TxtToImgApi {
