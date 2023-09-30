7841a664f41cda363fe9cc882091025df04070a2
노소래 <yessorae@gmail.com>
2023-09-30 15:09:24 +0900

    chore: ktlint

--- app/src/main/java/com/yessorae/imagefactory/ui/components/layout/EmptyListLayout.kt
+++ app/src/main/java/com/yessorae/imagefactory/ui/components/layout/EmptyListLayout.kt
@@ -2,7 +2,6 @@ package com.yessorae.imagefactory.ui.components.layout
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
