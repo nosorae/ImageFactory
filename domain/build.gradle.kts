6ee8b0ed77c9e5e970019d3e715355119091be05
yessorae <yessorae@gmail.com>
2024-01-27 20:54:59 +0900

    chore: domain 코루틴, 대거 어노테이션 의존성 추가

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -7,3 +7,9 @@ java {
     sourceCompatibility = JavaVersion.VERSION_1_7
     targetCompatibility = JavaVersion.VERSION_1_7
 }
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+    implementation("javax.inject:javax.inject:1")
+    testImplementation("junit:junit:4.13.2")
+}
